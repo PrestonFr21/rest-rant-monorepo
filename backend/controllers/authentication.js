2c0e3bf5ec204720e239af3f2f2e58195ac8f911
PrestonFr <PrestonFr@gmail.com>
2023-01-07 11:03:09 -0500

    fetching for login

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -3,9 +3,14 @@ const db = require("../models")
 const bcrypt = require('bcrypt')
 
 const { User } = db
-
+      
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    console.log(user)
 })
 
 module.exports = router
